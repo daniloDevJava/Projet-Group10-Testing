d9e95d04d20020aeda80691a90f63c71c3840ca1
daniloDevJava <danayomba69@gmail.com>
2025-06-14 09:45:22 +0100

    Resolution des conflits

--- vehicule/src/test/java/com/projet/testing/vehicule/VehiculeControllerTest.java
+++ vehicule/src/test/java/com/projet/testing/vehicule/VehiculeControllerTest.java
@@ -9,6 +9,7 @@ import com.aventstack.extentreports.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
